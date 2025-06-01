44878a40d949008148cae1aaa86a6b9f835d4c72
Björn Sterling <bjorn.sterling@gmail.com>
2025-06-01 14:50:27 +0200

    sudoecode for the function getCOmputerChoice is created

---
+++ javascript.js
@@ -0,0 +1,20 @@
+console.log("Page loaded!");
+
+// write a function with the name getComputerChoice. 
+
+// Crate a variable called result of the type string with the value "". 
+
+// create a variable called random of the type real with the staring value 0. 
+
+// Generate a random number between 0 and 1 and store it in random variable. 
+
+// Check is the variable is between 0 and 1/3 
+//      ->  if that is true update the value of result to "Rock" 
+
+// check if the variable is between 1/3 and 2/3
+//      -> if that is true update the value of result to "Paper"
+
+// else 
+//      -> update the value of result to "Rock"
+
+// return the value. 

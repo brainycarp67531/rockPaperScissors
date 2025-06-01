ca733e8eff6f27314f1ca8d057199e0cfaae8469
Björn Sterling <bjorn.sterling@gmail.com>
2025-06-01 17:01:46 +0200

    Comleted the sudocode for the function getHumanChoice

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ function getComputerChoice() {
 
     // Generate a random number between 0 and 1 and store it in random variable. 
     random = Math.random();
-    console.log(random);
+    // console.log(random);
 
     // Check is the variable is between 0 and 1/3 
     //      ->  if that is true update the value of result to "rock" 
@@ -32,3 +32,18 @@ function getComputerChoice() {
     // return the value. 
     return result;
 }
+
+// test the function getComputerChoice. 
+// let test = getComputerChoice();
+// console.log(test);
+
+
+// Write a function named getHumanChoice that will asked for input rock paper or scissors. 
+
+// Create a variable named humanChoice that will store the humans input.
+
+// Asked the human for input. 
+
+// Store the input in the variable
+
+// return the variable. 

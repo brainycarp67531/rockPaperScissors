78a9379c1d778db05ea5a413cff83cf00124e2eb
Björn Sterling <bjorn.sterling@gmail.com>
2025-06-01 16:52:20 +0200

    compleated the getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,20 +1,34 @@
 console.log("Page loaded!");
 
 // write a function with the name getComputerChoice. 
+function getComputerChoice() {
 
-// Crate a variable called result of the type string with the value "". 
+    // Crate a variable called result of the type string with the value "". 
+    let result = "";
 
-// create a variable called random of the type real with the staring value 0. 
+    // create a variable called random of the type real with the staring value 0. 
+    let random = 0;
 
-// Generate a random number between 0 and 1 and store it in random variable. 
+    // Generate a random number between 0 and 1 and store it in random variable. 
+    random = Math.random();
+    console.log(random);
 
-// Check is the variable is between 0 and 1/3 
-//      ->  if that is true update the value of result to "Rock" 
+    // Check is the variable is between 0 and 1/3 
+    //      ->  if that is true update the value of result to "rock" 
+    if (random < 1/3) {
+        result = "rock";
+    }
+    // check if the variable is between 1/3 and 2/3
+    //      -> if that is true update the value of result to "paper"
+    else if (random < 2/3) {
+        result = "paper";
+    }
+    // else 
+    //      -> update the value of result to "scissors"
+    else {
+        result = "scissors";
+    }
 
-// check if the variable is between 1/3 and 2/3
-//      -> if that is true update the value of result to "Paper"
-
-// else 
-//      -> update the value of result to "Rock"
-
-// return the value. 
+    // return the value. 
+    return result;
+}
